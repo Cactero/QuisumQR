96ab4789f9366d07a6a97d3fb5270cbd5821c569
Cactero <thatcactus@protonmail.com>
2023-04-13 20:46:36 +0800

    Post-scanning

---
+++ app/src/main/java/com/quisumbing/quisumqr/PostScan.kt
@@ -0,0 +1,48 @@
+package com.quisumbing.quisumqr
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import kotlinx.android.synthetic.main.activity_postscan.*
+import java.time.LocalDate
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+
+class PostScan : AppCompatActivity() {
+
+    private lateinit var database: DatabaseReference
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_postscan)
+
+        var loggedinStudent = intent.getStringExtra("loggedinStudent")
+        var scanTime = intent.getStringExtra("scanTime")
+
+        postscanTime.text = scanTime
+
+        val currentdate = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMdd"))
+        database =
+            FirebaseDatabase.getInstance("https://quisumqr-default-rtdb.asia-southeast1.firebasedatabase.app")
+                .getReference("Students")
+        if (loggedinStudent != null) {
+            database.child(loggedinStudent).child("Attendances").child(currentdate).get().addOnSuccessListener {
+                if (it.exists()){
+                    postscanTime.text = it.value.toString()
+                } else{
+                }}.addOnFailureListener {
+                Toast.makeText(this, "Failed", Toast.LENGTH_SHORT).show()
+
+                val btnToUserPrompt = findViewById<Button>(R.id.postscanGoToScanner)
+                btnToUserPrompt.setOnClickListener({
+                    val toUserPrompt = Intent(this, UserPrompt::class.java);
+                    startActivity((toUserPrompt));
+                    finish();
+                })
+            }
+        }
+}}
